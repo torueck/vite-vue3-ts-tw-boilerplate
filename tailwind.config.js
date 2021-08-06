a36166b534f484cedd2146145e07d703bac29146
torueck <torsten.ueckert@gmail.com>
2021-08-06 23:16:49 +0200

    chore: use tailwind JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  mode: 'jit',
+  purge: [
+    './index.html',
+    './public/**/*.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
