7a4cbf60096f97a2477b439fa4bf294a0b0e3906
Torsten Ueckert <40751707+torueck@users.noreply.github.com>
2021-12-23 19:46:19 +0100

    chore: bump dependencies (#3)
    * chore: bump dependencies
    
    * chore: bump vue
    
    * chore: bump jest
    
    * chore: bump testing-library
    
    * chore: bump eslint plugins and prettier
    
    * chore: bump dependencies

--- src/components/atoms/TBtn.vue
+++ src/components/atoms/TBtn.vue
@@ -1,23 +1,6 @@
 <template>
   <button
-    class="
-      px-4
-      py-2
-      font-medium
-      tracking-wide
-      text-white
-      capitalize
-      transition-colors
-      duration-200
-      transform
-      rounded-md
-      bg-blue-600
-      dark:bg-gray-800
-      hover:bg-blue-500
-      dark:hover:bg-gray-700
-      focus:outline-none focus:bg-blue-500
-      dark:focus:bg-gray-700
-    "
+    class="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform rounded-md bg-blue-600 dark:bg-gray-800 hover:bg-blue-500 dark:hover:bg-gray-700 focus:outline-none focus:bg-blue-500 dark:focus:bg-gray-700"
   >
     <slot></slot>
   </button>
